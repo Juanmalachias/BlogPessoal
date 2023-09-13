00a107c931a44f6639e980476b1a9673bc778bd6
Juan <malachias322@gmail.com>
2023-09-12 21:19:02 -0300

    Spring Security

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -0,0 +1,95 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_usuarios")
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatótio")
+	private String nome;
+	
+	@NotBlank(message = "O atributo usuario é obrigatótio")
+	@Email(message = "Tem que receber um email valido")
+	private String usuario;
+	
+	@NotBlank(message = "O atributo senha é obrigatótio")
+	@Size(min=8,message =" A senha tem que no minimo 8 caracteres")
+	private String senha;
+	
+	@Size(max=4000,message ="O link da foto não pode passar de 4000 caracteres")
+	private String foto;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="usuario", cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+}
